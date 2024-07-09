2fc2719460e573645cf7630a56e9c248df5621f3
siddiq9080 <147240577+siddiq9080@users.noreply.github.com>
2024-07-09 21:32:06 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Cards from './Cards/Card';
-//import Heads from "./Shooping-cart/Head-section/Head"
-// import Carts from './Shooping-cart/Head-section/Cart-items/Cart'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
  <Cards />
- {/* <Heads /> */}
- {/* <Carts /> */}
   </React.StrictMode>,
 )
